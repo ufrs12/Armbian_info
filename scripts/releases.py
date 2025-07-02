7c5145217d0bd14e122c7ea004f7e39d4a5d52c6
ufrs12 <ufrs12@yandex.ru>
2025-07-02 22:24:46 +0300

    1

--- scripts/releases.py
+++ scripts/releases.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 url = 'https://api.github.com/repos/armbian/build/releases'  # замените на нужный URL
 
@@ -15,6 +16,23 @@ except requests.RequestException as e:
 except ValueError:
     print('Ответ не содержит корректный JSON')
 
+obj = json.loads(rs)
+names = find_all_names(obj)
+print(names)
+
+def find_all_names(obj):
+    names = []
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if k == "name":
+                names.append(str(v))
+            else:
+                names.extend(find_all_names(v))
+    elif isinstance(obj, list):
+        for item in obj:
+            names.extend(find_all_names(item))
+    return names
+
 url = 'https://api.telegram.org/bot' + os.environ.get('TG_KEY') + '/sendmessage?chat_id=' + os.environ.get('TG_CHANNEL_ID') + '&text=' + rs
 try:
     response = requests.get(url)
@@ -25,3 +43,9 @@ except requests.RequestException as e:
     print(f'Ошибка запроса: {e}')
 except ValueError:
     print('Ответ не содержит корректный JSON')
+
+obj = json.loads(data)
+names_list = find_all_names(obj)
+result_str = ", ".join(names_list)
+
+print(result_str)  # Alice, Bob, Charlie
