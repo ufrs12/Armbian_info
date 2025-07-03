3e74817beeb7eb4e4ff35af97932f887e28e0855
ufrs12 <ufrs12@yandex.ru>
2025-07-03 16:17:14 +0300

    1

--- scripts/releases.py
+++ scripts/releases.py
@@ -3,28 +3,32 @@ import os
 import json
 
 url = 'https://api.github.com/repos/armbian/build/releases'  # замените на нужный URL
-
 try:
     response = requests.get(url)
     response.raise_for_status()  # проверка успешности запроса (код 200)
     data = response.json()       # парсим JSON из ответа
     first_item = next(iter(data), None)
+    name = first_item['name']
     print(first_item)
 except requests.RequestException as e:
     print(f'Ошибка запроса: {e}')
 except ValueError:
     print('Ответ не содержит корректный JSON')
-    
-name = first_item['name']
-body = 'https://github.com/armbian/build/releases/tag/'
 
-url = 'https://api.telegram.org/bot' + os.environ.get('TG_KEY') + '/sendmessage?chat_id=' + os.environ.get('TG_CHANNEL_ID') + '&text=' + name + ' \n' + body + name
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # проверка успешности запроса (код 200)
-    data = response.json()       # парсим JSON из ответа
-    print(data)
-except requests.RequestException as e:
-    print(f'Ошибка запроса: {e}')
-except ValueError:
-    print('Ответ не содержит корректный JSON')
+#with open('../current.json') as s:
+#  current = json.load(s)
+
+#if name != current[name]:
+#    body = 'https://github.com/armbian/build/releases/tag/'
+
+#    url = 'https://api.telegram.org/bot' + os.environ.get('TG_KEY') + '/sendmessage?chat_id=' + os.environ.get('TG_CHANNEL_ID') + '&text=' + name + ' \n' + body + name
+#    try:
+#        response = requests.get(url)
+#        response.raise_for_status()  # проверка успешности запроса (код 200)
+#        data = response.json()       # парсим JSON из ответа
+#        print(data)
+#    except requests.RequestException as e:
+#        print(f'Ошибка запроса: {e}')
+#    except ValueError:
+#        print('Ответ не содержит корректный JSON')
+#else:
