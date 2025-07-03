59958ac2acdd684be0ef18856bf585f023269f0e
ufrs12 <ufrs12@yandex.ru>
2025-07-03 16:32:36 +0300

    1

--- scripts/get_release.py
+++ scripts/get_release.py
@@ -7,9 +7,9 @@ try:
     response = requests.get(url)
     response.raise_for_status()  # проверка успешности запроса (код 200)
     data = response.json()       # парсим JSON из ответа
-    first_item = next(iter(data), None)
-    name = first_item['name']
-    print(first_item)
+    os.environ['CURRENT'] = next(iter(data), None)
+    os.environ['CURRENT_NAME'] = first_item['name']
+    print(first_item['name'])
 except requests.RequestException as e:
     print(f'Ошибка запроса: {e}')
 except ValueError:
