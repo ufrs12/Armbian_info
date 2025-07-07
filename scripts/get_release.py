2d55a1c4bc4c713e557a2dbaba9109f2a45f0c3e
ufrs12 <ufrs12@yandex.ru>
2025-07-07 20:53:53 +0300

    1

--- scripts/get_release.py
+++ scripts/get_release.py
@@ -6,24 +6,20 @@ with open('current.json') as s:
   current = json.load(s)
 
 url = 'https://api.github.com/repos/armbian/build/releases'  # замените на нужный URL
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # проверка успешности запроса (код 200)
-    data = response.json()       # парсим JSON из ответа
-    first_item = next(iter(data), None)
+
+response = requests.get(url)
+response.raise_for_status()  # проверка успешности запроса (код 200)
+data = response.json()       # парсим JSON из ответа
+first_item = next(iter(data), None)
     
-    github_env = os.environ.get('GITHUB_ENV')
-    if github_env:
-        with open(github_env, 'a') as f:
-            f.write(f"CURRENT={json.dumps(first_item)}\n")
-            f.write(f"CURRENT_NAME={first_item['name']}\n")
-            f.write(f"CURRENT_NAME_IN_REPO={current['name']}\n")
-    else:
-        print("Error: GITHUB_ENV variable not found")
+github_env = os.environ.get('GITHUB_ENV')
+if github_env:
+    with open(github_env, 'a') as f:
+        f.write(f"CURRENT={json.dumps(first_item)}\n")
+        f.write(f"CURRENT_NAME={first_item['name']}\n")
+        f.write(f"CURRENT_NAME_IN_REPO={current['name']}\n")
+else:
+    print("Error: GITHUB_ENV variable not found")
    
-    print('еременная курент' + os.environ['CURRENT'])
-    print(first_item['name'])
-except requests.RequestException as e:
-    print(f'Ошибка запроса: {e}')
-except ValueError:
-    print('Ответ не содержит корректный JSON')
+print('Переменная курент' + os.environ['CURRENT'])
+print(first_item['name'])
